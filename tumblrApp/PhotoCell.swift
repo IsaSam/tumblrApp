7210e2f6e3e919a67cdb9ae2edf2d7596552cbd7
IsaSam <isaacsamuel27@gmail.com>
2018-09-13 19:08:36 -0400

    Finish Hook up the Tumblr API

---
+++ tumblrApp/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  tumblrApp
+//
+//  Created by Isaac Samuel on 9/13/18.
+//  Copyright © 2018 Isaac Samuel. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- tumblrApp/View Controllers/PhotosViewController.swift
+++ tumblrApp/View Controllers/PhotosViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class PhotosViewController: UIViewController {
+   
+    @IBOutlet var tableView: UITableView!
+    
     
     //Initialize an empty array
     // 1.       2.             3.
@@ -16,6 +19,9 @@ class PhotosViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         
         // Network request snippet
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
@@ -36,6 +42,23 @@ class PhotosViewController: UIViewController {
         }
         task.resume()
 
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> Int {
+        return 5
+    }
+   /*
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "This is row \(indexPath.row)"
+        
+        return cell
+    }*/
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell") as! PhotoCell
+        
+        // Configure PhotoCell using the outlets that you've defined.
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
